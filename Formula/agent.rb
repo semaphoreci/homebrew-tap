c4eb36c5176ca00927bac9917871a3d6ea8ca63e
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-10 10:55:39 +0100

    Brew formula update for agent version v1.0.10

--- Formula/agent.rb
+++ Formula/agent.rb
@@ -2,16 +2,16 @@
 class Agent < Formula
   desc "Semaphore 2.0 agent."
   homepage "https://semaphoreci.com"
-  version "1.0.9"
+  version "1.0.10"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/semaphoreci/agent/releases/download/v1.0.9/agent_Darwin_x86_64.tar.gz"
-    sha256 "d0d3195f35097c1e7f46a2b9d8caf2cc0e33997f9919a060900545dd93d55662"
+    url "https://github.com/semaphoreci/agent/releases/download/v1.0.10/agent_Darwin_x86_64.tar.gz"
+    sha256 "82008e25f53113282f449afbaefb674f24b7b4bcc778c751eea0f6fcbe8e75f9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/semaphoreci/agent/releases/download/v1.0.9/agent_Linux_x86_64.tar.gz"
-      sha256 "84ee20145a504819ce4706935f8fc7d9aa438ad8dbee071beec0e7c19e34e05e"
+      url "https://github.com/semaphoreci/agent/releases/download/v1.0.10/agent_Linux_x86_64.tar.gz"
+      sha256 "5243f2e4663a23225d3804abe9a08913043d793ddabf97588d838627cf1af31f"
     end
   end
 
