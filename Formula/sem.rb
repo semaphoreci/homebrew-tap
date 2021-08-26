5ca436bbec596abd16248d9a6d013869f5a1a5e4
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-26 16:49:34 +0200

    Brew formula update for sem version v0.22.0

--- Formula/sem.rb
+++ Formula/sem.rb
@@ -2,9 +2,9 @@
 class Sem < Formula
   desc "Semaphore 2.0 command line interface."
   homepage "https://semaphoreci.com"
-  url "https://github.com/semaphoreci/cli/releases/download/v0.21.0/sem_Darwin_x86_64.tar.gz"
-  version "0.21.0"
-  sha256 "3986b89b39b6a071e45daaff8e4f207a28f7334215acd04c5e81c45fe1a0d589"
+  url "https://github.com/semaphoreci/cli/releases/download/v0.22.0/sem_Darwin_x86_64.tar.gz"
+  version "0.22.0"
+  sha256 "0fac6f61f65aac396d7ae8b6bf05688f0c97ddd026adb15aee1007cb095dddfb"
 
   def install
     bin.install "sem"
