161683b02af7c16134b2e727378e800823ab3a64
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-08 18:50:15 +0200

    Brew formula update for agent version v2.0.13

--- Formula/agent.rb
+++ Formula/agent.rb
@@ -5,28 +5,28 @@
 class Agent < Formula
   desc "Semaphore 2.0 agent."
   homepage "https://semaphoreci.com"
-  version "2.0.12"
+  version "2.0.13"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/semaphoreci/agent/releases/download/v2.0.12/agent_Darwin_x86_64.tar.gz"
-      sha256 "bb11b4e54fe6c29488cc939ba173e3c1a9d70151340b2ee1c6b6c6ae0244100f"
+      url "https://github.com/semaphoreci/agent/releases/download/v2.0.13/agent_Darwin_x86_64.tar.gz"
+      sha256 "e788fe22aba99c359f85c56d59655ac75dbc865d348904b7ba0187f114cb8bae"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/semaphoreci/agent/releases/download/v2.0.12/agent_Linux_x86_64.tar.gz"
-      sha256 "5064b019ba219ee26c2921766d8a32fb8fbc1a62b9ccbc9e76c8cec177012c93"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/semaphoreci/agent/releases/download/v2.0.13/agent_Linux_arm64.tar.gz"
+      sha256 "7bc73a3bd2295bc35dd7b568f0bdac19b98c1f09789be232bd0f8ae31c6149d5"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/semaphoreci/agent/releases/download/v2.0.12/agent_Linux_armv6.tar.gz"
-      sha256 "06f927defec06f1425a77b1e57392bebc092c4682330e5279a9fb98b7f0f92fc"
+      url "https://github.com/semaphoreci/agent/releases/download/v2.0.13/agent_Linux_armv6.tar.gz"
+      sha256 "07b0a9009721aba74cbcbf0a36a4d42c5922be83fdd74f8697e3b21094e6b9be"
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/semaphoreci/agent/releases/download/v2.0.12/agent_Linux_arm64.tar.gz"
-      sha256 "de5d4d3efb75bb56927a6757921fb5b6763d6ef7f8079150b0e6170ce4252bb7"
+    if Hardware::CPU.intel?
+      url "https://github.com/semaphoreci/agent/releases/download/v2.0.13/agent_Linux_x86_64.tar.gz"
+      sha256 "235c108987d6964c20a2b8eaffdabb0cb0aa0d877c198569915c99d564a512ab"
     end
   end
 
